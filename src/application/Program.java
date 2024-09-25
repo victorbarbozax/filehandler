5d105ecd37e820238c1ba01f1d50111ee4707521
Victor Barboza <victor.barboza052008@gmail.com>
2024-09-25 17:40:30 -0300

    Programa de crição de um novo arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,70 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		List<Product> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
+		String sourceFileStr = sc.nextLine();
+
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
 		
-		System.out.println("Enter a folder path: ");
-		String strPatch = sc.nextLine();
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
 		
-		File path = new File(strPatch);
-		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+				for (Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
 		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for(File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPatch + "\\subdir").mkdir();
-		System.out.println("Directory created succcessfully: " + success);
-		
+
 		sc.close();
 	}
 }
-
